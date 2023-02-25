d3ac215e15c3015addea87060554c46a6518036d
wjfatuan <wilson.forero@uan.edu.co>
2023-02-24 19:31:00 -0500

    ImageView

--- app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
+++ app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.ejemplowidgets0224
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.ejemplowidgets0224.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imgTeamShield.setImageResource(R.drawable.bart)
+        binding.btnLiverpool.setOnClickListener { changeTeamFlag("liverpool") }
+        binding.btnManchester.setOnClickListener { changeTeamFlag("manchester") }
+    }
+
+    fun changeTeamFlag(teamName:String) {
+        val id = resources.getIdentifier(teamName, "drawable", packageName)
+        binding.imgTeamShield.setImageResource(id)
     }
 }
