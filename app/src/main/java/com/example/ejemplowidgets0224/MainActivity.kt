afaff767607830867c8e76d20bf8265adb2978dc
wjfatuan <wilson.forero@uan.edu.co>
2023-03-03 18:41:43 -0500

    Adding a static ListView

--- app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
+++ app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ejemplowidgets0224
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.example.ejemplowidgets0224.databinding.ActivityMainBinding
 
@@ -15,10 +16,26 @@ class MainActivity : AppCompatActivity() {
         binding.imgTeamShield.setImageResource(R.drawable.bart)
         binding.btnLiverpool.setOnClickListener { changeTeamFlag("liverpool") }
         binding.btnManchester.setOnClickListener { changeTeamFlag("manchester") }
+        binding.rdbLiverpool.setOnClickListener { changeTeamFlag("liverpool") }
+        binding.rdbManchester.setOnClickListener { changeTeamFlag("manchester") }
+        binding.switchTeam.setOnClickListener {
+            if(binding.switchTeam.isChecked)
+                changeTeamFlag("liverpool")
+            else
+                changeTeamFlag("manchester")
+        }
+        binding.teamsList.setOnItemClickListener { list, view, position, id -> // lambda
+            println( "list: $list \n" +
+                    "view: $view \n" +
+                    "position: $position \n" +
+                    "id: $id")
+            val tv = view as TextView
+            changeTeamFlag(tv.text.toString().lowercase())
+        }
     }
 
     fun changeTeamFlag(teamName:String) {
-        val id = resources.getIdentifier(teamName, "drawable", packageName)
+        val id = resources.getIdentifier(teamName, "drawable", packageName) // R.drawable.teanName
         binding.imgTeamShield.setImageResource(id)
     }
 }
