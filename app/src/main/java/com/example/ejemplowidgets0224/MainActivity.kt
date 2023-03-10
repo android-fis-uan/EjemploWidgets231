81af85ac00188a08b9dbddde6f21f95f32fe1ce0
wjfatuan <wilson.forero@uan.edu.co>
2023-03-10 18:39:44 -0500

    Using intents with other activity

--- app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
+++ app/src/main/java/com/example/ejemplowidgets0224/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ejemplowidgets0224
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -10,11 +11,14 @@ import com.example.ejemplowidgets0224.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
+    lateinit var adapter:TeamsListAdapter
     val teamNames = arrayListOf<String>("Liverpool","Manchester")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        println(intent)
         binding.imgTeamShield.setImageResource(R.drawable.bart)
         binding.btnLiverpool.setOnClickListener { changeTeamFlag("liverpool") }
         binding.btnManchester.setOnClickListener { changeTeamFlag("manchester") }
@@ -27,19 +31,28 @@ class MainActivity : AppCompatActivity() {
                 changeTeamFlag("manchester")
         }
         // conectar la lista con el arreglo de datos usando un adaptador
-        val adapter = TeamsListAdapter(this, R.layout.list_teams, teamNames )
+        adapter = TeamsListAdapter(this, R.layout.list_teams, teamNames )
         binding.teamsList.adapter = adapter
         binding.teamsList.setOnItemClickListener { list, view, position, id -> // lambda
             println( "list: $list \n" +
                     "view: $view \n" +
                     "position: $position \n" +
                     "id: $id")
-            changeTeamFlag(teamNames[position].lowercase())
+            //changeTeamFlag(teamNames[position].lowercase())
+            openTeamInfoActivity(teamNames[position])
         }
     }
 
     fun changeTeamFlag(teamName:String) {
         val id = resources.getIdentifier(teamName, "drawable", packageName) // R.drawable.teanName
         binding.imgTeamShield.setImageResource(id)
+        teamNames.add("Otro equipo")
+        adapter.notifyDataSetChanged()
+    }
+
+    fun openTeamInfoActivity(teamName: String) {
+        val teamInfoIntent = Intent( this, InfoEquipoActivity::class.java)
+        teamInfoIntent.putExtra("TEAM_NAME", teamName)
+        startActivity(teamInfoIntent)
     }
 }
